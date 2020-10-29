ee7ab6784dcd4403abb6616d80befdb7da9d8f84
Guillem Delas <guillemdelas@hotmail.com>
2020-10-30 00:43:07 +0100

    implemented some views and worked on some behaviour from the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,31 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = policy_scope(Post).order(created_at: :desc)
+    @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments
   end
 
   def new
     @post = Post.new
-    authorize @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:description)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,6 @@ class User < ApplicationRecord
 
   validates :username, :email, presence: true
   validates :full_name, presence: true
-  validates :full_name, format: { with: /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/,
-  message: 'it only allows letters' }
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
