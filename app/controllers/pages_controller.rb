415f5897d4e189aa7732d3b5619e7183a7db2afe
Guillem Delas <guillemdelas@hotmail.com>
2020-11-02 13:09:16 +0100

    added a page to see all the users of the app and a search bar (not implemented yet)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home; end
+
+  def index
+    @users = User.all
+    @user = current_user
+  end
 end
