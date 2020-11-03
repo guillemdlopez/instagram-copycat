921aabef09d26e657b7d80af3adbb3270f243fa4
Guillem Delas <guillemdelas@hotmail.com>
2020-11-03 18:08:54 +0100

    added the geocoder gem and the location column to the posts table

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,6 @@ class Post < ApplicationRecord
 
   has_one_attached :photo
 
-  validates :photo, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_31_163659) do
+ActiveRecord::Schema.define(version: 2020_11_03_170033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,9 @@ ActiveRecord::Schema.define(version: 2020_10_31_163659) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "location"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
